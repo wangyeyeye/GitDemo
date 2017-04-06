1f5de4933b5120d02d31dd91fa8a7372d9938d7a
wangyeyeye <995857315@qq.com>
2017-04-06 11:13:08 +0800

    2222

--- src/com/git/Demo.java
+++ src/com/git/Demo.java
@@ -2,4 +2,12 @@ package com.git;
 
 public class Demo {
 	private String demo = "demo";
+
+	public String getDemo() {
+		return demo;
+	}
+
+	public void setDemo(String demo) {
+		this.demo = demo;
+	}
 }
